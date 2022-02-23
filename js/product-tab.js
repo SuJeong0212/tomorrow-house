0fc2c89524eafaf32a8925d7471771605697b991
SuJeong0212 <s.rystal0212@gmail.com>
2022-02-23 14:58:16 +0900

    💫 Scroll to tab-pannel when product-tab-item is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,18 +1,34 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
 let currentActiveTab = productTab.querySelector('.is-active')
 
-function toggleActiveTab(){
-    const tabItem = this.parentNode
+function toggleActiveTab() {
+  const tabItem = this.parentNode
 
-    if(currentActiveTab !== tabItem){
-        tabItem.classList.add('is-active')
-        currentActiveTab.classList.remove('is-active')
-        currentActiveTab = tabItem
-    }
+  if (currentActiveTab !== tabItem) {
+    tabItem.classList.add('is-active')
+    currentActiveTab.classList.remove('is-active')
+    currentActiveTab = tabItem
+  }
+}
+
+function scrollToTabPannel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount = tabPanel.getBoundingClientRect().top - (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
 }
 
 productTabButtonList.forEach((button) => {
-    button.addEventListener('click',toggleActiveTab)
+  button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPannel)
 })
