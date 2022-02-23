977928b288210c6ef180709b32eabb1f77f69674
SuJeong0212 <s.rystal0212@gmail.com>
2022-02-23 16:52:44 +0900

    🚧 Detect position of tab-panels

--- js/product-tab.js
+++ js/product-tab.js
@@ -20,7 +20,9 @@ function scrollToTabPannel() {
   const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
   const tabPanel = document.querySelector(`#${tabPanelId}`)
 
-  const scrollAmount = tabPanel.getBoundingClientRect().top - (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
 
   window.scrollBy({
     top: scrollAmount,
@@ -32,3 +34,30 @@ productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
   button.addEventListener('click', scrollToTabPannel)
 })
+
+const productTabPanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation',
+]
+const productTabPanelList = productTabPanelIdList.map((panelId) => {
+  const tabPanel = document.querySelector(`#${panelId}`)
+  return tabPanel
+})
+const productTabPanelPositionMap = {}
+
+function detectTabPanelPosition() {
+  //각각의 tabPanel의 y축 위치를 찾는다
+  //productTabPanelPositionMap에 그 값을 업데이트
+
+  productTabPanelList.forEach((panel) => {
+    const id = panel.getAttribute('id')
+    const position = window.scrollY + panel.getBoundingClientRect().top
+    productTabPanelPositionMap[id] = position
+  })
+}
+
+window.addEventListener('load', detectTabPanelPosition)
+window.addEventListener('resize', detectTabPanelPosition)
